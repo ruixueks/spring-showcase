a565bbd197f0273cdf59665d874e348956a779a6
ruixue <ruixue.zzz@qq.com>
2017-11-10 15:34:03 +0800

    ..
    Signed-off-by: ruixue <ruixue.zzz@qq.com>

--- src/main/java/com/cheer/spring/aop/ex01/CalcLoggingAspect.java
+++ src/main/java/com/cheer/spring/aop/ex01/CalcLoggingAspect.java
@@ -10,8 +10,10 @@ package com.cheer.spring.aop.ex01;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,9 +25,31 @@ public class CalcLoggingAspect
     //标识这个方法是个前置通知,切点表达式表示执行 ArithmeticCalculator接口的 add()方法 
     // * 代表匹配任意修饰符及任意返回值,
     //参数列表中的 .. 匹配任意数量的参数
-    @Before("execution(* com.cheer.spring.aop.impl.ArithmeticCalculator.add(..))")
+//    @Before("execution(* com.cheer.spring.aop.impl.ArithmeticCalculator.add(..))")
+//    public void logBefore()
+//    {
+//        LOGGER.info("The method add() begins");
+//    }
+//    
+//    @After("execution(* com.cheer.spring.aop.impl.ArithmeticCalculator.add(..))")
+//    public void logAfter()
+//    {
+//        LOGGER.info("The method add() ends");
+//    }
+    
+    @Pointcut("execution(* com.cheer.spring.aop.impl.ArithmeticCalculator.add(..))")
+    private void loggingOperation()
+    {}
+    
+    @Before("loggingOperation()")
     public void logBefore()
     {
         LOGGER.info("The method add() begins");
+    }
+    
+    @After("loggingOperation()")
+    public void logAfter()
+    {
+        LOGGER.info("The method add() ends");
     }
 }
